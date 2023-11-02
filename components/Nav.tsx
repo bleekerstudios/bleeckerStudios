ca89476baec08453ac225c84bb0f31e26d713b1f
bleekerstudios <sofi@bleekerstudios.com>
2023-11-01 21:09:15 -0400

    Update Nav.tsx

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -37,7 +37,7 @@ const Nav = () => {
           onClick={() => setIsNavOpen(!isNavOpen)}
           data-collapse-toggle="navbar-default"
           type="button"
-          className="inline-flex items-center p-2 ml-3 text-black md:hidden hover:bg-primary-100 focus:outline-none focus:ring-2 focus:ring-primary-200 "
+          className="inline-flex items-center p-2 ml-3 text-black md:hidden hover:bg-white focus:outline-none "
           aria-controls="navbar-dropdown"
           aria-expanded="false"
         >
@@ -46,17 +46,17 @@ const Nav = () => {
         </button>
         <div
           // ref={menu}
-          className={` ${isNavOpen ? 'block' : 'hidden'} menu w-full md:block md:w-auto absolute w-full left-0 md:w-auto md:relative`}
+          className={` ${isNavOpen ? 'block' : 'hidden'} menu w-full md:block md:w-auto absolute left-0 md:w-auto md:relative `}
           id="navbar-default"
         >
-          <ul className="my-4 md:my-0 p-4 md:p-0 flex font-medium gap-6 flex-col border border-gray-100 bg-gray-50 md:flex-row md:mt-0 md:font-medium md:border-0 md:bg-white  ">
+          <ul className="my-4 md:my-0 p-4 md:p-0 flex font-medium gap-6 flex-col border border-gray-100 bg-gray-50 md:flex-row md:mt-0 md:font-medium md:border-0 md:bg-white ">
             {navigation.map((item) => (
               <li key={item.name}>
                 <Link
                   passHref
                   key={item.name}
                   href={item.href}
-                  className={` block py-2 pl-3 pr-4 transition-colors duration-300 rounded md:bg-transparent md:text-black md:hover:text-primary-500 md:p-0 md:p-0
+                  className={` block py-2 pl-3 pr-4 transition-colors duration-300 rounded md:bg-transparent md:text-black md:hover:text-primary-500 md:p-0 md:p-0 
                     ${router.pathname === item.href
                       ? "text-white bg-primary-700 "
                       : "text-black"
