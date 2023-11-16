a24dc9d1e5009d007f3ce60c4c7c8c9aceb7dde2
bleekerstudios <sofi@bleekerstudios.com>
2023-11-15 22:44:52 -0500

    Update browseServices.tsx

--- components/browseServices.tsx
+++ components/browseServices.tsx
@@ -13,7 +13,7 @@ const DATA = [
     
     {
       title: 'Browse Our Common Services',
-      content: `Our suite of out-of-the-box services has driven efficiency and cost savings across a range of enterprises`,
+      content: `Our suite of services has driven efficiency and cost savings across a range of enterprises.`,
       image: '/images/gtube.svg',
       imageAltTag: 'highlight-3',
       icon: 'lucide:folder-search',
